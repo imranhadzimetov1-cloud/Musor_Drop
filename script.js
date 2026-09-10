458d7d840cbc72c679f02c57ba2ecc9a10603444
KingBaton <imranhadzimetov1@gmail.com>
2026-09-10 22:36:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -107,14 +107,14 @@ function generateXabibTexture() {
 // 4. БАЗА СКИНОВ (С XABIB)
 // ==========================================
 const SKINS_DATABASE = [
-    { id: 101, weapon: "P250", name: "P250 | Fallen", rarity: "RARE", price: 2, img: "p250.png" },
-    { id: 2, weapon: "Glock-18", name: "Glock-18 | Bunsen Burner", rarity: "COMMON", price: 3 },
-    { id: 3, weapon: "AK-47", name: "AK-47 | Safari Mesh", rarity: "COMMON", price: 5 },
-    { id: 4, weapon: "M4A4", name: "M4A4 | Mainframe", rarity: "COMMON", price: 6 },
-    { id: 5, weapon: "USP-S", name: "USP-S | Torque", rarity: "COMMON", price: 4 },
-    { id: 103, weapon: "MP9", name: "MP9 | Starlight", rarity: "RARE", price: 3, img: "mp9.png" },
-    { id: 7, weapon: "P90", name: "P90 | Traction", rarity: "COMMON", price: 4 },
-    { id: 8, weapon: "Galil AR", name: "Galil AR | Vandal", rarity: "COMMON", price: 5 },
+    { id: 101, weapon: "P250", name: "P250 | Fallen", rarity: "COMMON", price: 3, img: "p250.png" },
+    { id: 108, weapon: "Glock-18", name: "Glock-18 | Fade", rarity: "COMMON", price: 7, img: "glock-fade.png" },
+    { id: 111, weapon: "AK-47", name: "AK-47 | Jungle Diamond", rarity: "COMMON", price: 20, img: "ak-jungle.png" },
+    { id: 113, weapon: "M4A1-S", name: "M4A1-S | Mecha Industries", rarity: "COMMON", price: 450, img: "m4a1-mecha.png" },
+    { id: 109, weapon: "USP-S", name: "USP-S | Printstream", rarity: "COMMON", price: 6, img: "usp-printstream.png" },
+    { id: 103, weapon: "MP9", name: "MP9 | Starlight", rarity: "COMMON", price: 8, img: "mp9.png" },
+    { id: 110, weapon: "P90", name: "P90 | Asiimov", rarity: "COMMON", price: 4, img: "p90-asiimov.png" },
+    { id: 112, weapon: "Galil AR", name: "Galil AR | Cerberus", rarity: "COMMON", price: 5, img: "galil-camo.png" },
     { id: 21, weapon: "AK-47", name: "AK-47 | Emerald Pinstripe", rarity: "RARE", price: 25 },
     { id: 22, weapon: "M4A1-S", name: "M4A1-S | Flashback", rarity: "RARE", price: 35 },
     { id: 23, weapon: "AWP", name: "AWP | Worm God", rarity: "RARE", price: 45 },
